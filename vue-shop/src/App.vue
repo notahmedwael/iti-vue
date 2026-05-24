1a478e718c508709dd260761092bffc7cbc133df
Ahmed Wael <notahmedwael888@gmail.com>
2026-05-24 18:01:42 +0300

    updated discount for item with id 3

--- vue-shop/src/App.vue
+++ vue-shop/src/App.vue
@@ -32,7 +32,7 @@ const products = ref([
     "image": "https://images.unsplash.com/photo-1600185365926-3a2ce3cdb9eb?q=80&w=1025&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
     "badge": "SALE",
     "price": 150,
-    "discount": 0,
+    "discount": 40,
     "stock": 8,
     "tags": ["Casual", "Winter"]
   },
